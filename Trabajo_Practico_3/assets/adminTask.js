e0b7ac4e009e03ca976f6870f5cd3f85a267a8bf
Gaston Florenciano <gaston__florenciano@hotmail.com>
2024-05-18 18:14:29 -0300

    FINALIZACIÓN DEL TRABAJO

--- Trabajo_Practico_3/assets/adminTask.js
+++ Trabajo_Practico_3/assets/adminTask.js
@@ -1,62 +1,101 @@
 const regex = /^\S+$/;
 let tasks = [];
 
-function listarTareas(){
+function listarTareas() {
 
-    if(tasks.length > 0){
+    if (tasks.length > 0) {
         alert(tasks);
-    }else{
-        alert('Todavía no hay materias');
+    } else {
+        alert('Todavía no hay tareas');
     }
 }
 
-function agregarTarea(){
-    
-    let add = prompt('Ingrese una materia para añadir:');
-    
-    if(regex.test(add)){
+function agregarTarea() {
+
+    let add = prompt('Ingrese una Tarea para añadir:');
+
+    if (regex.test(add)) {
         tasks.push(add.toUpperCase());
-        alert('Materia añadida!');
-    }else{
-        alert('Error');
+        alert('Tarea añadida!');
+    } else {
+        alert('No puede estar vacío o contener espacios al principio/final');
     }
 }
 
+function editarTarea() {
 
+    if (tasks.length > 0) {
 
-function eliminarMateria() {
-    let deleteTask = prompt('Ingrese la materia que quiera eliminar:');
-    if (deleteTask) {
-        let index = tasks.indexOf(deleteTask.toUpperCase());
-        if (index !== -1) {
-            tasks.splice(index, 1);
-            alert('Materia eliminada!');
+        let tarea = prompt('Ingrese la Tarea que quiera editar:');
+        let nuevo = prompt('Ingrese la nueva Tarea:')
+        
+        if(regex.test(nuevo)){
+
+            if (tasks.includes(tarea.toUpperCase())) {
+    
+                let index = tasks.indexOf(tarea);
+    
+                tasks.splice(index, 1, nuevo.toUpperCase())
+    
+                alert('Tarea editada!');
+    
+            } else {
+                alert('Tarea no encontrada');
+            }
+        }else{
+            alert('No puede estar vacío o contener espacios al principio/final');
+        }
+
+    } else {
+        alert('Todavía no hay Tareas');
+    }
+}
+
+function eliminarTarea() {
+    if (tasks.length > 0) {
+
+        let tarea = prompt('Ingrese la Tarea que quiera eliminar:');
+
+        if(regex.test(tarea)){
+
+            if (tasks.includes(tarea.toUpperCase())) {
+                let index = tasks.indexOf(tarea.toUpperCase());
+                tasks.splice(index, 1);
+                alert('Tarea eliminada!');
+            }else {
+                alert('Tarea no encontrada');
+            }
         } else {
-            alert('Error: Materia no encontrada.');
+            alert('No puede estar vacío o contener espacios al principio/final');
         }
     } else {
-        alert('No se eliminó ninguna materia.');
+        alert('Todavía no hay Tareas');
     }
 }
 
-function mostrarMenu(){
-    while (true){
-        let actions = parseInt(prompt('Ingrese el número de la acción que quiera realizar:' + '\n1-LISTAR MATERIAS' + '\n2-AÑADIR MATERIA' + '\n3-EDITAR MATERIA' + '\n4-ELIMINAR MATERIA'));
+function mostrarMenu() {
+    while (true) {
+        let actions = parseInt(prompt('Ingrese el número de la acción que quiera realizar:' + '\n1-LISTAR Tareas' + '\n2-AÑADIR Tarea' + '\n3-EDITAR Tarea' + '\n4-ELIMINAR Tarea' + '\n5-SALIR: Se eliminarán las tareas existentes.'));
 
         switch (actions) {
             case 1:
-                listTask();
+                listarTareas();
                 break;
             case 2:
-                addTask();
+                agregarTarea();
                 break;
             case 3:
-                editarMateria();
+                editarTarea();
                 break;
             case 4:
-                eliminarMateria();
+                eliminarTarea();
+                break;
+            case 5:
+                alert('Saliendo del sistema...');
+                tasks = [];
                 break;
             default:
+                alert('Ingrese un numero que corresponda a las opciones');
                 break;
         }
     }
